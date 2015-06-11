159dc7ed26f54bf86fd9c0a9dfe837cd887cb42c
李向军 <xjli@thinkjoy.cn>
2015-06-11 19:41:46 +0800

    修复问题级联删除

--- src/main/java/cn/ld/fj/util/Config.java
+++ src/main/java/cn/ld/fj/util/Config.java
@@ -5,7 +5,9 @@ package cn.ld.fj.util;
  */
 public class Config {
     //  public static String back_url = "/Users/xjli/test/";
-    public static String back_url = "/var/lib/tomcat7/webapps/backup/";
-    public static String access_url = "http://120.24.161.74:8080/backup/";
+//    public static String back_url = "/var/lib/tomcat7/webapps/backup/";
+//    public static String access_url = "http://120.24.161.74:8080/backup/";
+    public static String back_url = "C:/Users/Administrator/Downloads/apache-tomcat-7.0.62-windows-x86/apache-tomcat-7.0.62/webapps/backup/";
+    public static String access_url = "http://115.29.48.102:8080/backup/";
 
 }
